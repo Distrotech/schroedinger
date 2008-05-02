83bf54e0326cf027c8ed7ab1d4f46d16882ac587
Bart Wiegmans <bart@darkstar.(none)>
2008-05-02 21:26:09 +0200

    First wave intact

--- java/TestWavelet2d.java
+++ java/TestWavelet2d.java
@@ -3,7 +3,6 @@ import org.diracvideo.Schroedinger.*;
 public class TestWavelet2d {
     public static void main(String a[]) {
 	System.out.println("Hello, Wavelets!");
-	short d[] = new short[16*16];
-	WaveletTransform.inverse(d,16);
+	WaveletTransform.test();
     }
 }
--- java/org/diracvideo/Schroedinger/WaveletTransform.java
+++ java/org/diracvideo/Schroedinger/WaveletTransform.java
@@ -3,44 +3,33 @@ package org.diracvideo.Schroedinger;
 public class WaveletTransform {
     public static void inverse(short d[], int w) {
 	/* data is assumed to be preinterleaved */
-	int h = d.length / w;
-	for(int s = 8; s  > 0; s >>= 1) {
-	    for(int x = 0; x < w; x++) {
-		synthesize(d, x, w*x*s, -1); /* all columns */
-	    }
-	    for(int y = 0; y < h; y++) {
-		synthesize(d,y*w,s,-1); /* all rows */
-	    }
-	}
-	/* shift bits */
 	for(int i = 0; i < d.length; i++) {
 	    d[i] += 1;
 	    d[i] >>= 1;
 	}
     }
 
-    private static void synthesize(short data[], int start,
-				   int stride, int width) {
-	for(int i = 0; i < width; i += stride) {
+    private static void synthesize(short d[]) {
+	for(int i = 0; i < d.length; i += 2) {
 	    if(i == 0) {
-		data[0] -= (data[1] + 2) >> 2;
-	    } else if (i == width - 1) {
-		data[2*i] -= (data[2*i-1] + 2) >> 2;
+		d[0] -= (2*d[1] + 2) >> 2;
+	    } else if (i + 1 == d.length) {
+		d[i] -= (2*d[i-1] + 2) >> 2;
 	    } else {
-		data[2*i] -= (data[2*i-1] + data[2*i+1] + 2) >> 2;
+		d[i] -= (d[i-1] + d[i+1] + 2) >> 2;
 	    }
 	}
-	for(int i = 0; i < width; i += stride) {
-	    if(i+1 > width) {
-		data[2*i+1] += (data[2*i] + 1) >> 1;
-	    } else { 
-		data[2*i+1] += (data[2*i] + data[2*i+2] + 1) >> 1;
+	for(int i = 0; i < d.length; i += 2) {
+	    if(i + 2 >= d.length) {
+		d[i+1] += (2*d[i] + 1) >> 1;
+	    } else {
+		d[i+1] += (d[i] + d[i+2] + 1) >> 1;
 	    }
 	}
     }
 
     public static short[] interleave(short ll[], short hl[], 
-			      short lh[], short hh[], int w) {
+				     short lh[], short hh[], int w) {
 	short o[] = new short[ll.length*4];
 	for(int i = 0; i < ll.length; i++) {
 	    for(int j = 0; j < ll.length; j++) {
@@ -54,5 +43,13 @@ public class WaveletTransform {
 	return o;
     }
 
+    public static void test() {
+	short d[] = {0,0,2,0,4,0,6,0,8,1};
+	synthesize(d);
+	for(int i = 0; i < d.length; i++) {
+	    System.out.print(" " + d[i]);
+	}
+	System.out.println("");
+    }
     
 }
