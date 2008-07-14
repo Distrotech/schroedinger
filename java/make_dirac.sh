a13a6d3b42620d3019ab2110d74871b5974dbdd7
Bart Wiegmans <bart@darkstar.(none)>
2008-07-14 22:28:17 +0200

    Added make dirac script

---
+++ java/make_dirac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gst-launch videotestsrc ! schroenc intra_wavelet=1 \
+	    enable_noarith=true transform_depth=4  \
+	    gop_structure=1 rate_control=3 ! \
+	     filesink location=test.drc
