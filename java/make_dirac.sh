9eade53b1a08819ca347d5fe3246f78ca9b49903
Bart Wiegmans <bart@darkstar.(none)>
2008-08-24 22:46:41 +0200

    Fixed bug in mv_precision=2

--- java/make_dirac.sh
+++ java/make_dirac.sh
@@ -3,8 +3,8 @@
 LD_LIBRARY_PATH=/usr/local/lib
 gst-launch videotestsrc ! schroenc intra_wavelet=6 \
     inter_wavelet=1 enable_noarith=false transform_depth=4  \
-    gop_structure=0 rate_control=3 mv_precision=1 \
-    motion_block_overlap=0 ! filesink location=test.drc &
+    gop_structure=0 rate_control=3 mv_precision=2 \
+    motion_block_overlap=1 ! filesink location=test.drc &
 sleep 5
 kill -s 2 %gst-launch
 exit
